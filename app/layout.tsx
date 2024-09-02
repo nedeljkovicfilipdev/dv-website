85197eaa8e1ad4599baa9eaf5493fedee76da75d
Filip Nedeljković <nedeljkovic.filip.dev@gmail.com>
2024-09-02 18:17:24 +0200

    Bg color and text color

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: Props) {
         {/* Add any global meta tags or links here */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-white text-black`}>
         {children}
       </body>
     </html>
