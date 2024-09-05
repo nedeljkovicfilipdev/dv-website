39bd9bb1faf4270d864b1510cc21099fe4f8a9d0
Filip Nedeljković <nedeljkovic.filip.dev@gmail.com>
2024-09-05 15:45:19 +0200

    Changes to url image sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: Props) {
         {/* Open Graph Meta Tags */}
         <meta property="og:title" content="Diverse Visa - Expert Immigration Services for Canada and Beyond" />
         <meta property="og:description" content="Expert immigration services for Canada. Diverse Visa helps with visa applications, study permits, work permits, and more. Seamless, professional, and successful outcomes." />
-        <meta property="og:image" content="/icon.ico" /> {/* Replace with a specific image if needed */}
+        <meta property="og:image" content="/images/logo/dv-logo-reverse.jpg" /> {/* Replace with a specific image if needed */}
         <meta property="og:url" content="https://diversevisa.com" />
 
         {/* Viewport Meta Tag */}
