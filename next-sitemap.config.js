743589c13a4c6e312aa4e5a0569e0c7e47fb9d2b
Filip Nedeljković <nedeljkovic.filip.dev@gmail.com>
2024-09-20 20:02:53 +0200

    Added sitemap for website

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.diversevisa.com', // Your domain
+    generateRobotsTxt: true, // (optional) Generate robots.txt file
+    // Optional settings:
+    exclude: ['/admin/*'],
+  }
+  
